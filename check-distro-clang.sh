5cd26fc39474d4e581a78e0ea550e8c6319a0b15
Nathan Chancellor <nathan@kernel.org>
2021-01-29 11:54:15 -0700

    misc-scripts: Add check-distro-clang.sh
    This script allows us to easily check the version of clang available
    through various distributions.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ check-distro-clang.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Make sure we have the required binaries
+for NAME in podman docker; do
+    command -v "${NAME}" &>/dev/null && BINARY=${NAME}
+done
+if [[ -z ${BINARY} ]]; then
+    echo "Neither podman nor docker could be found on your system! Please install one to use this script."
+    exit 1
+fi
+
+DOCKER_DISTROS=(
+    archlinux/base:latest
+    debian:stable-slim
+    debian:testing-slim
+    debian:unstable-slim
+    fedora:latest
+    fedora:rawhide
+    opensuse/leap:latest
+    opensuse/tumbleweed:latest
+    ubuntu:18.04
+    ubuntu:20.04
+    ubuntu:latest
+    ubuntu:rolling
+    ubuntu:devel
+)
+
+BASE=$(dirname "$(readlink -f "${0}")")
+RUN_SCRIPT=${BASE}/run.sh
+
+cat <<'EOF' >"${RUN_SCRIPT}"
+#!/usr/bin/env bash
+
+RESULTS=$(dirname "$(readlink -f "${0}")")/results.log
+
+set -x
+
+# Debian/Ubuntu
+if command -v apt-get &>/dev/null; then
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+    DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y clang
+# Fedora
+elif command -v dnf &>/dev/null; then
+    dnf update -y
+    dnf install -y clang
+# Arch
+elif command -v pacman &>/dev/null; then
+    pacman -Syyu --noconfirm
+    pacman -S --noconfirm clang
+# OpenSUSE Leap/Tumbleweed
+elif command -v zypper &>/dev/null; then
+    zypper -n up
+    zypper -n in clang
+fi
+
+echo "${1}: $(clang --version | head -n1)" >> "${RESULTS}"
+EOF
+chmod +x "${RUN_SCRIPT}"
+
+rm "${BASE}"/results.log
+for DISTRO in "${DOCKER_DISTROS[@]}"; do
+    "${BINARY}" pull "${DISTRO}"
+    "${BINARY}" run \
+        --rm \
+        --init \
+        --volume="${BASE}:${BASE}" \
+        --workdir="${BASE}" \
+        "${DISTRO}" \
+        "${BASE}"/run.sh "${DISTRO}"
+done
+
+echo
+cat "${BASE}"/results.log
