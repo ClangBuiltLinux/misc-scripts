114ceefb67e2d91e17fe87651eeaa4f8333e2e21
Nathan Chancellor <nathan@kernel.org>
2021-11-19 14:32:32 -0700

    get-distro-clang.sh: Update the list and add comment around bumping versions
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- distro-clang/get-distro-clang.sh
+++ distro-clang/get-distro-clang.sh
@@ -11,19 +11,52 @@ if [[ -z ${BINARY} ]]; then
     exit 1
 fi
 
+# Useful links for bumping versions
+#
+# Debian:
+#   * https://www.debian.org/releases/
+#   * https://wiki.debian.org/LTS
+#   * https://hub.docker.com/_/debian
+#
+# Fedora:
+#   * https://fedoraproject.org/wiki/Releases
+#   * https://fedoraproject.org/wiki/End_of_life
+#   * https://hub.docker.com/_/fedora
+#
+# OpenSUSE:
+#   * https://en.opensuse.org/openSUSE:Roadmap
+#   * https://en.opensuse.org/Lifetime
+#   * https://hub.docker.com/r/opensuse/leap
+#
+# Ubuntu:
+#   * https://wiki.ubuntu.com/Releases
+#   * https://hub.docker.com/_/ubuntu
+#
+# This list should only include versions that are actively being supported.
+#
+# Tags such as "latest", "stable", or "rolling" are preferred so that the list
+# does not have to be constantly updated. Old but supported releases like
+# Fedora or OpenSUSE are the exception.
 DOCKER_DISTROS=(
-    archlinux/archlinux
+    archlinux:latest
+
+    debian:oldoldstable-slim
     debian:oldstable-slim
     debian:stable-slim
     debian:testing-slim
     debian:unstable-slim
-    fedora
+
+    fedora:34
+    fedora:latest
     fedora:rawhide
-    opensuse/leap
-    opensuse/tumbleweed
-    ubuntu:18.04
-    ubuntu:20.04
-    ubuntu
+
+    opensuse/leap:15.2
+    opensuse/leap:latest
+    opensuse/tumbleweed:latest
+
+    ubuntu:bionic
+    ubuntu:latest
+    ubuntu:hirsute
     ubuntu:rolling
     ubuntu:devel
 )
