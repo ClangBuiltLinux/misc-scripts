9bcaded82df6fee2d9a2ede52daf0d3dca54b321
Nathan Chancellor <nathan@kernel.org>
2021-11-19 14:41:48 -0700

    install-check-clang-version.sh: Export DEBIAN_FRONTEND instead of prefixing to apt-get
    This makes reading the commands a little easier.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- distro-clang/install-check-clang-version.sh
+++ distro-clang/install-check-clang-version.sh
@@ -6,9 +6,11 @@ set -x
 
 # Debian/Ubuntu
 if command -v apt-get &>/dev/null; then
+    export DEBIAN_FRONTEND=noninteractive
+
     apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-    DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y clang
+    apt-get upgrade -y
+    apt-get install --no-install-recommends -y clang
 # Fedora
 elif command -v dnf &>/dev/null; then
     dnf update -y
